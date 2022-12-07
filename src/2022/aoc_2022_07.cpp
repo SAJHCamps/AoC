39162a87e56f73a1ae91c1513ea5b17befba6838
Sean Camps <S.A.J.H.Camps@student.tudelft.nl>
2022-12-07 14:43:07 +0100

    2022 day 7 part 2

--- src/2022/aoc_2022_07.cpp
+++ src/2022/aoc_2022_07.cpp
@@ -9,6 +9,7 @@
 #include <tuple>
 #include <openssl/md5.h>
 #include <bits/stdc++.h>
+#include <algorithm>
 
 struct file_tree {
     bool is_dir;
@@ -104,6 +105,22 @@ void run_2022_7_part_1(bool test) {
     file.close();
 }
 
+int check_sizes_file_tree_part_2(struct file_tree* start, int to_free) {
+    if (start->is_dir) {
+        int smallest = INFINITY;
+        if (start->size > to_free) {
+            smallest = start->size;
+        }
+        for (auto i: start->children) {
+            smallest = std::min(smallest, check_sizes_file_tree_part_2(i, to_free));
+        }
+        return smallest;
+    }
+    else {
+        return INFINITY;
+    }
+}
+
 void run_2022_7_part_2(bool test) {
     std::ifstream file;
     std::cout << "Part 2 day 7" << std::endl;
@@ -117,23 +134,49 @@ void run_2022_7_part_2(bool test) {
         file.open("../src/2022/input/day_7.txt");
     }
     std::string input;
-    std::getline(file, input);
-    int found = 0;
-    std::set<char> current;
-    for (int i = 0; i < 13; i++) {
-        current.insert(input[i]);
-    }
-    for (int i = 0; i < input.length()-14; i++) {
-        std::set<char> current;
-        for (int j = i; j < 14+i; j++) {
-            current.insert(input[j]);
+    auto root = new struct file_tree;
+    root->name = "/";
+    root->parent = nullptr;
+    root->is_dir = true;
+    root->size = -1;
+    struct file_tree *current;
+    current = root;
+    while (std::getline(file, input)) {
+        if (input[0] != '$') {
+            auto new_file = new struct file_tree;
+            new_file->parent = current;
+            new_file->children = {};
+            if (input[0] >= '0' && input[0] <= '9') {
+                int begin_name = input.find_first_not_of("0123456789");
+                new_file->size = std::stoi(input.substr(0, begin_name));
+                new_file->name = input.substr(begin_name + 1, std::string::npos);
+                new_file->is_dir = false;
+            } else {
+                new_file->size = -1;
+                new_file->name = input.substr(4, std::string::npos);
+                new_file->is_dir = true;
+            }
+            current->children.push_back(new_file);
+        } else {
+            if (input[2] == 'c') {
+                if (input[5] == '.') {
+                    current = current->parent;
+                } else if (input[5] == '/') {
+                    current = root;
+                } else {
+                    for (auto i: current->children) {
+                        if (i->is_dir && i->name == input.substr(5, std::string::npos)) {
+                            current = i;
+                            break;
+                        }
+                    }
+                }
+            }
         }
-        if (current.size() == 14) {
-            found = i + 14;
-            break;
-        }
     }
-    std::cout << found << std::endl;
+    set_sizes_file_tree(root);
+
+    std::cout << check_sizes_file_tree_part_2(root,  root->size - 40000000) << std::endl;
     file.close();
 }
 
