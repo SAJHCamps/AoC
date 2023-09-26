c8b1ddb455bd7fb86f2609bdf042d00e9ceee808
Sean Camps <S.A.J.H.Camps@student.tudelft.nl>
2023-09-27 01:03:32 +0200

    2016 day 1 part 2

--- src/2016/aoc_2016_01.cpp
+++ src/2016/aoc_2016_01.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <set>
 
 void run_2016_1_part_1(bool test) {
     std::ifstream file;
@@ -55,7 +56,7 @@ void run_2016_1_part_2(bool test) {
     std::ifstream file;
     std::cout << "Part 1 day 1" << std::endl;
     if (test) {
-        file.open("../src/2016/test/test_1_1.txt");
+        file.open("../src/2016/test/test_1_2.txt");
         std::string answer;
         std::getline(file,answer);
         std::cout << "Test should give: " << answer << std::endl;
@@ -64,8 +65,72 @@ void run_2016_1_part_2(bool test) {
         file.open("../src/2016/input/day_1.txt");
     }
     std::string input;
-    std::getline(file,input);
-    std::cout << input << std::endl;
+    int x = 0;
+    int y = 0;
+    int direction = 0;
+    std::set<std::pair<int,int>> visited;
+    visited.insert(std::make_pair(x,y));
+    while (std::getline(file, input, ',')) {
+        if (input.find('R') != std::string::npos){
+            direction = (direction + 1) % 4;
+        }
+        else if (input.find('L') != std::string::npos){
+            direction = (direction + 3) % 4;
+        }
+        int start = input.find_first_of("0123456789");
+        int end = input.find_first_not_of("0123456789", start);
+        int amount  = std::stoi(input.substr(start, end));
+        switch (direction) {
+            case 0:
+                for (int _ = 0; _ < amount; _++){
+                    x++;
+                    if (visited.find(std::make_pair(x,y)) != visited.end()) {
+                        goto found;
+                    }
+                    else {
+                        visited.insert(std::make_pair(x,y));
+                    }
+                }
+                break;
+            case 1:
+                for (int _ = 0; _ < amount; _++){
+                    y++;
+                    if (visited.find(std::make_pair(x,y)) != visited.end()) {
+                        goto found;
+                    }
+                    else {
+                        visited.insert(std::make_pair(x,y));
+                    }
+                }
+                break;
+            case 2:
+                for (int _ = 0; _ < amount; _++){
+                    x--;
+                    if (visited.find(std::make_pair(x,y)) != visited.end()) {
+                        goto found;
+                    }
+                    else {
+                        visited.insert(std::make_pair(x,y));
+                    }
+                }
+                break;
+            case 3:
+                for (int _ = 0; _ < amount; _++){
+                    y--;
+                    if (visited.find(std::make_pair(x,y)) != visited.end()) {
+                        goto found;
+                    }
+                    else {
+                        visited.insert(std::make_pair(x,y));
+                    }
+                }
+                break;
+            default:
+                std::cout << "I have a bad feeling about this" << std::endl;
+        }
+    }
+    found:
+    std::cout << abs(x) + abs(y) << std::endl;
     file.close();
 }
 
