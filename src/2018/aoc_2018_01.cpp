c081e99b945a160cbffd30e57b5fcf70d18d3d93
Sean Camps <S.A.J.H.Camps@student.tudelft.nl>
2023-09-27 11:50:46 +0200

    2018 day 1 part 2

--- src/2018/aoc_2018_01.cpp
+++ src/2018/aoc_2018_01.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <set>
 
 void run_2018_1_part_1(bool test) {
     std::ifstream file;
@@ -45,8 +47,33 @@ void run_2018_1_part_2(bool test) {
         file.open("../src/2018/input/day_1.txt");
     }
     std::string input;
-    std::getline(file,input);
-    std::cout << input << std::endl;
+    int current = 0;
+    std::vector<int> changes;
+    std::set<int> seen;
+    seen.insert(current);
+    while (std::getline(file,input)) {
+        if (input[0] == '+') {
+            changes.push_back(stoi(input.substr(1,std::string::npos)));
+        }
+        else if (input[0] == '-') {
+            changes.push_back(stoi(input));
+        }
+        else {
+            std::cout << "I have a bad feeling about this" << std::endl;
+        }
+    }
+    int i = 0;
+    while (true) {
+        current += changes[i];
+        if (seen.find(current) != seen.end()) {
+            break;
+        }
+        else {
+            seen.insert(current);
+        }
+        i = (i+1) % changes.size();
+    }
+    std::cout << current << std::endl;
     file.close();
 }
 
