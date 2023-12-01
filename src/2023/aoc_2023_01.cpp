7c34b3decd7a116cc8b64b369fb2eea3ccf25c42
Sean Camps <S.A.J.H.Camps@student.tudelft.nl>
2023-12-01 23:14:05 +0100

    2023 day 1 part 2

--- src/2023/aoc_2023_01.cpp
+++ src/2023/aoc_2023_01.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 void run_2023_1_part_1(bool test) {
     std::ifstream file;
@@ -44,8 +45,28 @@ void run_2023_1_part_2(bool test) {
         file.open("../src/2023/input/day_1.txt");
     }
     std::string input;
-    std::getline(file,input);
-    std::cout << input << std::endl;
+    std::string input_parsed;
+    int score = 0;
+    std::vector<std::string> literals = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    while(std::getline(file,input)) {
+        input_parsed = input;
+        for (int i = 0; i <= 8; i++) {
+            int index = input.find(literals[i]);
+            while (index != std::string::npos) {
+                input_parsed.replace(index, 1, std::to_string(i+1));
+                index = input.find(literals[i], index+1);
+            }
+        }
+        int first = input_parsed.find_first_of("0123456789");
+        int last = input_parsed.find_first_of("0123456789", first+1);
+        int second_last = first;
+        while (last != std::string::npos) {
+            second_last = last;
+            last = input_parsed.find_first_of("0123456789", last+1);
+        }
+        score += 10*(input_parsed[first]- '0') + input_parsed[second_last] - '0';
+    }
+    std::cout << score << std::endl;
     file.close();
 }
 
