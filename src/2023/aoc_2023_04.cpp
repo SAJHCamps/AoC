f8e2162d903f568c527b92d3f26ddc39b965c5f0
Sean Camps <S.A.J.H.Camps@student.tudelft.nl>
2023-12-04 10:20:06 +0100

    2023 day 4 part 1

--- src/2023/aoc_2023_04.cpp
+++ src/2023/aoc_2023_04.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 void run_2023_4_part_1(bool test) {
     std::ifstream file;
@@ -17,50 +18,37 @@ void run_2023_4_part_1(bool test) {
         file.open("../src/2023/input/day_4.txt");
     }
     std::string input;
-    std::vector<std::string> schematic;
-    while(std::getline(file,input)) {
-        schematic.push_back(input);
-    }
     int score = 0;
-    for (int i = 0; i < schematic.size(); i++) {
-        int current_symbol = schematic[i].find_first_of("0123456789");
-        while (current_symbol != std::string::npos) {
-            int end = current_symbol;
-            while ('0' <= schematic[i][end] && '9' >= schematic[i][end] && end < schematic[i].size())
-                end++;
-            end--;
-            bool connected = false;
-            for (int j = -1; j<2; j++) {
-                for (int k = -1; k<= end- current_symbol +1; k++) {
-                    if (i+j >= 0 && i+j < schematic.size() && current_symbol+k >= 0 && current_symbol+k < schematic[0].size()) {
-                        if ((schematic[i+j][current_symbol+k] < '0' || schematic[i+j][current_symbol+k] > '9') && schematic[i+j][current_symbol+k] != '.')
-                            connected = true;
-                    }
-                }
+    while(std::getline(file,input)) {
+        std::unordered_set<int> winners;
+        int begin  = input.find_first_of(':');
+        int seperator = input.find_first_of('|');
+        int begin_number = input.find_first_of("1234567890", begin);
+        int end_number = input.find_first_not_of("1234567890", begin_number);
+        while (begin_number < seperator) {
+            winners.insert(std::stoi(input.substr(begin_number, end_number-begin_number)));
+            begin_number = input.find_first_of("1234567890", end_number);
+            end_number = input.find_first_not_of("1234567890", begin_number);
+        }
+        int count = 0;
+        begin_number = input.find_first_of("1234567890", seperator);
+        end_number = input.find_first_not_of("1234567890", begin_number);
+        while (begin_number != std::string::npos) {
+            if (winners.find(std::stoi(input.substr(begin_number, end_number-begin_number))) != winners.end())
+                count++;
+            begin_number = input.find_first_of("1234567890", end_number);
+            end_number = input.find_first_not_of("1234567890", begin_number);
+        }
+        if (count > 0){
+            int card_score = 1;
+            for (int i =0; i< count-1; i++) {
+                card_score = card_score*2;
             }
-            if (connected)
-                score += stoi(schematic[i].substr(current_symbol, end - current_symbol+1));
-            current_symbol = schematic[i].find_first_of("0123456789", end+1);
+            score += card_score;
         }
     }
     std::cout << score << std::endl;
     file.close();
-}
-
-int find_number(std::string line, int pos) {
-    if (pos < 0 && pos >= line.size())
-        return -1;
-    if (line[pos] < '0' || line[pos] > '9')
-        return -1;
-    int end = pos;
-    while ('0' <= line[end] && '9' >= line[end] && end < line.size())
-        end++;
-    end--;
-    int begin = pos;
-    while ('0' <= line[begin] && '9' >= line[begin] && begin >= 0)
-        begin--;
-    begin++;
-    return std::stoi(line.substr(begin, end - begin+1));
 }
 
 void run_2023_4_part_2(bool test) {
@@ -76,55 +64,7 @@ void run_2023_4_part_2(bool test) {
         file.open("../src/2023/input/day_4.txt");
     }
     std::string input;
-    std::vector<std::string> schematic;
-    while(std::getline(file,input)) {
-        schematic.push_back(input);
-    }
-    int score = 0;
-    for (int i = 0; i < schematic.size(); i++) {
-        int current_symbol = schematic[i].find_first_of('*');
-        while (current_symbol != std::string::npos) {
-            std::vector<int> found;
-            if (i > 0) {
-                if (schematic[i-1][current_symbol] >= '0' && schematic[i-1][current_symbol] <= '9') {
-                    found.push_back(find_number(schematic[i-1], current_symbol));
-                }
-                else {
-                    int left_number = find_number(schematic[i-1], current_symbol-1);
-                    int right_number = find_number(schematic[i-1], current_symbol+1);
-                    if (left_number > 0)
-                        found.push_back(left_number);
-                    if (right_number > 0)
-                        found.push_back(right_number);
-                }
-            }
-            if (i < schematic.size()-1) {
-                if (schematic[i+1][current_symbol] >= '0' && schematic[i+1][current_symbol] <= '9') {
-                    found.push_back(find_number(schematic[i+1], current_symbol));
-                }
-                else {
-                    int left_number = find_number(schematic[i+1], current_symbol-1);
-                    int right_number = find_number(schematic[i+1], current_symbol+1);
-                    if (left_number > 0)
-                        found.push_back(left_number);
-                    if (right_number > 0)
-                        found.push_back(right_number);
-                }
-            }
-            int left_number = find_number(schematic[i], current_symbol-1);
-            int right_number = find_number(schematic[i], current_symbol+1);
-            if (left_number > 0)
-                found.push_back(left_number);
-            if (right_number > 0)
-                found.push_back(right_number);
-
-            if (found.size() == 2) {
-                score += found[0]*found[1];
-            }
-            current_symbol = schematic[i].find_first_of('*', current_symbol+1);
-        }
-    }
-    std::cout << score << std::endl;
+    std::cout << "not done" << std::endl;
     file.close();
 }
 
