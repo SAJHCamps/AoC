7de4bda44386d7fa34ea6f07fe47ceb0b3756d1c
Sean Camps <S.A.J.H.Camps@student.tudelft.nl>
2023-12-04 10:33:49 +0100

    2023 day 4 part 2

--- src/2023/aoc_2023_04.cpp
+++ src/2023/aoc_2023_04.cpp
@@ -64,7 +64,41 @@ void run_2023_4_part_2(bool test) {
         file.open("../src/2023/input/day_4.txt");
     }
     std::string input;
-    std::cout << "not done" << std::endl;
+    std::vector<std::string> cards;
+    std::vector<int> amount_cards;
+    while (std::getline(file,input)) {
+        cards.push_back(input);
+        amount_cards.push_back(1);
+    }
+    for (int card_index = 0; card_index < cards.size(); card_index++) {
+        std::unordered_set<int> winners;
+        int begin  = cards[card_index].find_first_of(':');
+        int seperator = cards[card_index].find_first_of('|');
+        int begin_number = cards[card_index].find_first_of("1234567890", begin);
+        int end_number = cards[card_index].find_first_not_of("1234567890", begin_number);
+        while (begin_number < seperator) {
+            winners.insert(std::stoi(cards[card_index].substr(begin_number, end_number-begin_number)));
+            begin_number = cards[card_index].find_first_of("1234567890", end_number);
+            end_number = cards[card_index].find_first_not_of("1234567890", begin_number);
+        }
+        int count = 0;
+        begin_number = cards[card_index].find_first_of("1234567890", seperator);
+        end_number = cards[card_index].find_first_not_of("1234567890", begin_number);
+        while (begin_number != std::string::npos) {
+            if (winners.find(std::stoi(cards[card_index].substr(begin_number, end_number-begin_number))) != winners.end())
+                count++;
+            begin_number = cards[card_index].find_first_of("1234567890", end_number);
+            end_number = cards[card_index].find_first_not_of("1234567890", begin_number);
+        }
+        for (int i = 1; i <= count; i++) {
+            amount_cards[card_index+i] += amount_cards[card_index];
+        }
+    }
+
+    int score = 0;
+    for (int amount: amount_cards)
+        score += amount;
+    std::cout << score << std::endl;
     file.close();
 }
 
