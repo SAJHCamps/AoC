02a5090f4f4d3a0dcd28c46bf4718529236512ab
SAJHCamps <sean.camps@gmail.com>
2023-12-05 10:54:26 +0100

    2023 day 5 part 1

--- src/2023/aoc_2023_05.cpp
+++ src/2023/aoc_2023_05.cpp
@@ -4,6 +4,17 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
+
+struct map {
+    long long start_input;
+    long long start_output;
+    long long range;
+};
+
+bool map_compare(map a, map b) {
+    return a.start_input < b.start_input;
+}
 
 void run_2023_5_part_1(bool test) {
     std::ifstream file;
@@ -18,36 +29,53 @@ void run_2023_5_part_1(bool test) {
         file.open("../src/2023/input/day_5.txt");
     }
     std::string input;
-    int score = 0;
-    while(std::getline(file,input)) {
-        std::unordered_set<int> winners;
-        int begin  = input.find_first_of(':');
-        int seperator = input.find_first_of('|');
-        int begin_number = input.find_first_of("1234567890", begin);
-        int end_number = input.find_first_not_of("1234567890", begin_number);
-        while (begin_number < seperator) {
-            winners.insert(std::stoi(input.substr(begin_number, end_number-begin_number)));
-            begin_number = input.find_first_of("1234567890", end_number);
-            end_number = input.find_first_not_of("1234567890", begin_number);
-        }
-        int count = 0;
-        begin_number = input.find_first_of("1234567890", seperator);
-        end_number = input.find_first_not_of("1234567890", begin_number);
-        while (begin_number != std::string::npos) {
-            if (winners.find(std::stoi(input.substr(begin_number, end_number-begin_number))) != winners.end())
-                count++;
-            begin_number = input.find_first_of("1234567890", end_number);
-            end_number = input.find_first_not_of("1234567890", begin_number);
-        }
-        if (count > 0){
-            int card_score = 1;
-            for (int i =0; i< count-1; i++) {
-                card_score = card_score*2;
-            }
-            score += card_score;
+    std::getline(file, input);
+    std::vector<long long> points;
+    int current = input.find_first_of("0123456789");
+    int end = input.find_first_not_of("0123456789", current);
+    while (current != std::string::npos) {
+        points.push_back(std::stoll(input.substr(current,end - current)));
+        current = input.find_first_of("0123456789", end);
+        end = input.find_first_not_of("0123456789", current);
+    }
+
+    std::vector<std::vector<map>> conversions;
+    for (int i = 0; i < 7; i++) {
+        conversions.push_back({});
+    }
+    int current_map = -1;
+
+    while (std::getline(file, input)) {
+        if (input.find(':') != std::string::npos) {
+            current_map++;
+        } else if (input[0] >= '0' && input[0] <= '9') {
+            map entry;
+            current = input.find_first_of("0123456789");
+            end = input.find_first_not_of("0123456789", current);
+            entry.start_output =  std::stoll(input.substr(current,end - current));
+            current = input.find_first_of("0123456789", end);
+            end = input.find_first_not_of("0123456789", current);
+            entry.start_input =  std::stoll(input.substr(current,end - current));
+            current = input.find_first_of("0123456789", end);
+            end = input.find_first_not_of("0123456789", current);
+            entry.range =  std::stoll(input.substr(current,end - current));
+            conversions[current_map].push_back(entry);
         }
     }
-    std::cout << score << std::endl;
+    for (int i = 0; i < 7; i++) {
+        std::sort(conversions[i].begin(), conversions[i].end(), map_compare);
+        for (int j = 0; j < points.size(); j++) {
+            int begin = conversions[i].size()-1;
+            while (conversions[i][begin].start_input > points[j] && begin >= 1)
+                begin--;
+
+            if (conversions[i][begin].start_input <= points[j]
+                    && conversions[i][begin].start_input + conversions[i][begin].range > points[j])
+                points[j] = conversions[i][begin].start_output - conversions[i][begin].start_input + points[j];
+        }
+    }
+
+    std::cout << *std::min_element(points.begin(), points.end()) << std::endl;
     file.close();
 }
 
